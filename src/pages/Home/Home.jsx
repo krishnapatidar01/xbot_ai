de74b345ccb2346fb0b24d2a034bf579e2cd7f0b
Krishna <krishnapatidar1971@gmail.com>
2025-06-11 17:00:50 +0530

    first commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -67,6 +67,18 @@ export default function Home() {
         >
 
             <Navbar />
+             <Typography
+                variant="h1"
+                sx={{
+                    fontSize: '2rem',
+                    fontWeight: 'bold',
+                    textAlign: 'center',
+                    mt: 2,
+                    color: mode === 'light' ? '#000' : '#fff'
+                }}
+            >
+                Bot AI
+            </Typography>
 
             {chat.length == 0 && <InitialChat generateResponse={generateResponse} />}
 
